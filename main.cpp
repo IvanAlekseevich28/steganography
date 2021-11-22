9c088cf68994f690b9434fad3265b39e1553124c
Ivan <ivaz3823@gmail.com>
2021-11-22 23:36:49 +0300

    First version

--- main.cpp
+++ main.cpp
@@ -1,46 +1,143 @@
 #include <iostream>
+#include <algorithm>
 #include "bmp.h"
 #include "steganography.h"
 #include "file_converter.h"
 
 using namespace std;
 
+string getCmdOption(char ** begin, char ** end, const std::string & option)
+{
+    char ** itr = std::find(begin, end, option);
+    if (itr != end && ++itr != end)
+    {
+        return string(*itr);
+    }
+    return "";
+}
+
+void printHelp()
+{
+    cout << "\t-s           source file\n"
+         << "\t'-h'/'-e'    hide/extract mode\n"
+         << "\t-f           file for hide\n"
+         << "\t-o           file for output\n"
+         << "\t-b           extrapolate bits to bytes\n"
+         << "\t-t           text mod\n";
+}
+
+string getDir(const string& path)
+{
+    auto iter = path.find_last_of("/");
+    if (iter == string::npos)
+        return "";
+    else
+        return path.substr(0,iter+1);
+}
+
 int main(int argc, char** argv)
 {
+    if (getCmdOption(argv, argv+argc, "--help") != "")
+    {
+        printHelp();
+        return 0;
+    }
     if (argc < 4)
     {
         cout << "Not enought arguments!\n";
+        printHelp();
         return 1;
     }
-    const string mod = argv[1];
-    const string pathCont = argv[2];
-    const string pathOutput = argv[3];
+    const bool mod = getCmdOption(argv, argv+argc, "-h") != ""; // 1 - hide, 0 - extract
+    const bool needExt = getCmdOption(argv, argv+argc, "-b") != "";
+    const string text = getCmdOption(argv, argv+argc, "-t");
+    const string pathHide = getCmdOption(argv, argv+argc, "-f");
+    const string pathSrc = getCmdOption(argv, argv+argc, "-s");
+    const string pathOutput = getCmdOption(argv, argv+argc, "-o");
 
-    if (isFileExists(pathCont) == false)
+    if (pathSrc.empty() or !isFileExists(pathSrc))
     {
-        cout << "File \"" << pathCont << "\" not correct!\n";
+        cout << "File \"" << pathSrc << "\" not correct!\n";
         return 2;
     }
 
-    if (mod == "-h" or mod == "--hide")
+    BMP pic(pathSrc.c_str());
+    string data;
+    if (mod)
     {
-        const string dataMod = argv[4];
+        if (text.size())
+        {
+            data = text;
+        } else if (pathHide.size() and isFileExists(pathHide))
+        {
+            data = fileToStr(pathHide);
+        } else
+        {
+            cout << "Nothing for doing!\n";
+            return 4;
+        }
 
-    } else if (mod == "-e" or mod == "--extract")
-    {
+        if (hideMessage(pic.data, data) == false)
+        {
+            cout << "Message too lenght!/nMax: "
+                 << getMaxByteLen(pic.data)
+                 << "\nCurrent: "
+                 << data.size() << "\n";
+            return 8;
+        }
 
+        if (pathOutput.empty())
+        {
+            cout << "Output path is empty!\n";
+            return 16;
+        }
+
+        pic.write(pathOutput.c_str());
+        if (isFileExists(pathOutput) == false)
+        {
+            cout << "Output path is not correct!\n";
+            return 32;
+        }
+
+        if (needExt)
+        {
+            string pathExtOrig = getDir(pathOutput) + "ext_orig.bmp";
+            string pathExtHide = getDir(pathOutput) + "ext_hide.bmp";
+
+            extrapolateLastBitsToBytes(pic.data);
+            pic.write(pathExtHide.c_str());
+
+            pic.read(pathSrc.c_str());
+            extrapolateLastBitsToBytes(pic.data);
+            pic.write(pathExtOrig.c_str());
+
+        }
     } else
     {
-        cout << "Select mode\n'-h' to hide data\n'-e' to extract data\n";
-        return 4;
-    }
-//    BMP pic("sample_640×426.bmp");
-//    cout << hideMessage(pic.data, "Hello world!") << endl;
-//    pic.write("test.bmp");
+        string data = extractHideMessage(pic.data);
+        if (data.empty())
+        {
+            cout << "No hidden data for " << pathSrc << "\n";
+            return 128;
+        }
 
-//    BMP pic2("test.bmp");
-//    cout << extractHideMessage(pic2.data) << endl;
-    auto data = fileToStr("pp.pptx");
-    strToFile("str.pptx", data);
+        if (text.size())
+        {
+            cout << data << endl;
+            return 0;
+        } else if (pathOutput.size())
+        {
+            strToFile(pathOutput, data);
+            if (isFileExists(pathOutput))
+            {
+                return 0;
+            }
+        } else
+        {
+            cout << "Output problem!\n";
+            return 256;
+        }
+    }
+
     return 0;
 }
